b05bd887be4a2104f386a05fee2d53ce36c70493
shivaniharjani <shivanih06@gmail.com>
2022-08-23 11:30:52 -0400

    Created ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Just 5 week of JAVA left!");
+        System.out.println("Just 9 days left till my 3 year anniversary");
+    }
+}

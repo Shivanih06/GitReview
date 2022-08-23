8bc27a8dbac1e29cdd323f038d1d9684317e75fa
shivaniharjani <shivanih06@gmail.com>
2022-08-23 11:31:30 -0400

    Edited ByeWorld.java

--- src/first/ByeWorld.java
+++ src/first/ByeWorld.java
@@ -4,6 +4,9 @@ public class ByeWorld {
     public static void main(String[] args) {
 
         System.out.println("Just 5 week of JAVA left!");
-        System.out.println("Just 9 days left till my 3 year anniversary");
+        System.out.println("1.5 weeks of git!");
+
+
+
     }
 }
